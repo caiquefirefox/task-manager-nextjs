08987aababf3c77ae9c2eb52089bf8a50e3e31fb
Caique S. Soares <caiqueoucaique@hotmail.com>
2022-10-11 21:23:47 -0300

    Add docker-compose file

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -10,12 +10,26 @@ export default (
   res: NextApiResponse
 ) => {
     try{
+        if(req.method !== 'POST'){
+            return res.status(405).json({error: 'Metodo solicitado não existe'})
+        }
+
         const {body} = req;
         const dados = body as CadastroRequest;
 
         if(!dados.email || !dados.password){
             res.status(400).json({error: 'Favor preencher os campos'});
             return;
+        }
+
+        const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if(!filter.test(dados.email)){
+            return res.status(400).json({error: 'Email inválido'});
+        }
+
+        const senhaRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/;
+        if(!senhaRegex.test(dados.password)){
+            return res.status(400).json({error: 'Senha inválida'});
         }
 
         res.status(200).json({ msg: 'Cadastro concluido com sucesso' })
