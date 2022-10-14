8b7f0440d5e204c7482220149ff8a517aa7a6bbb
Caique S. Soares <caiqueoucaique@hotmail.com>
2022-10-13 21:44:38 -0300

    Adding login page and images/icons

---
+++ containers/Login.tsx
@@ -0,0 +1,20 @@
+import type {NextPage} from 'next';
+
+export const Login : NextPage = () => {
+  return (
+      <div>
+        <img src="/logo.svg" alt="Logo Fiap" />
+        <div className='form'>
+            <div>
+                <img src="/mail.svg" alt="Login" />
+                <input type="text" placeholder="Login" />
+            </div>
+            <div>
+                <img src="/lock.svg" alt="Senha" />
+                <input type="password" placeholder="Senha" />
+            </div>
+            <button type='button'>Login</button>
+        </div>
+      </div>
+  );
+}
