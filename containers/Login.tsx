abbe81da9d97c297d86fc130ac069cf6d668a5f7
Caique S. Soares <caiqueoucaique@hotmail.com>
2022-10-20 21:16:02 -0300

    Adding functions on the login page

--- containers/Login.tsx
+++ containers/Login.tsx
@@ -3,16 +3,24 @@ import type {NextPage} from 'next';
 import { useState } from 'react';
 import { executeRequest } from '../services/api';
 
-export const Login : NextPage = () => {
+type LoginProps = {
+  setAccessToken(s: string) : void
+}
+
+export const Login : NextPage<LoginProps> = ({setAccessToken}) => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const doLogin = async () => { 
     try {
       if (!email || !password) {
-        alert('Favor, preencha todos os campos');
+        return setError('Favor, preencha todos os campos');
       }
+
+      setLoading(true);
 
       const body ={
         login: email,
@@ -22,17 +30,30 @@ export const Login : NextPage = () => {
       const result = await executeRequest('login', 'POST', body);
 
       if (result && result.data) {
-        alert(result.data);
+        localStorage.setItem('accessToken', result.data.token);
+        localStorage.setItem('name', result.data.name);
+        localStorage.setItem('email', result.data.email);
+        setAccessToken(result.data.token);
       }
-    } catch (error) {
-      console.error('Ocorreu um erro ao efetuar login: ', error);
+
+    } catch (error : any) {
+      console.log('Ocorreu um erro ao efetuar login: ', error);
+
+      if (error?.response?.data?.error) {
+        setError(error?.response?.data?.error);
+      }else{
+        setError('Ocorreu um erro ao efetuar login, tente novamente mais tarde');
+      }
     }
+
+    setLoading(false);
   }
 
   return (
       <div className='container-login'>
         <img src="/logo.svg" alt="Logo Fiap" className='logo'/>
         <div className='form'>
+          {error && <p className='error'>{error}</p>}
             <div>
                 <img src="/mail.svg" alt="Login" />
                 <input type="text" placeholder="Login" 
@@ -43,7 +64,7 @@ export const Login : NextPage = () => {
                 <input type="password" placeholder="Senha" 
                   value={password} onChange={e => setPassword(e.target.value)} />
             </div>
-            <button type='button' onClick={doLogin}>Login</button>
+            <button type='button' onClick={doLogin} disabled={loading}>{loading ? 'Carregando...' : 'Login'}</button>
         </div>
       </div>
   );
